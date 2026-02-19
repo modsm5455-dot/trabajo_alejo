c0213a1cf67c83f0894d1a7d3c517790c707c04e
unknown <estebanosorio690@gmail.com>
2026-02-19 09:29:12 -0500

    fix: use MYSQL_URL railway

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,5 @@
 import mysql from "mysql2/promise";
-console.log("ENV CHECK:", process.env.DB_HOST);
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-});
+console.log("MYSQL_URL:", process.env.MYSQL_URL);
+
+export const db = mysql.createPool(process.env.MYSQL_URL as string);
