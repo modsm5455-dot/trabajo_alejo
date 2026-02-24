79c35efe7305a370d283e3b88b6527b27627e912
unknown <estebanosorio690@gmail.com>
2026-02-24 06:58:56 -0500

    fix railway mysql connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,9 @@
 import mysql from "mysql2/promise";
 
+console.log("HOST:", process.env.MYSQLHOST);
+console.log("USER:", process.env.MYSQLUSER);
+console.log("DATABASE:", process.env.MYSQLDATABASE);
+
 export const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
