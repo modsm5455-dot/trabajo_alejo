84749d816496ece33815a58de018f0915cb054a2
unknown <estebanosorio690@gmail.com>
2026-02-24 07:07:57 -0500

    fix railway mysql connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,9 @@
 import mysql from "mysql2/promise";
 
-console.log("HOST:", process.env.DB_HOST);
-console.log("USER:", process.env.DB_USER);
-console.log("DATABASE:", process.env.DB_NAME);
-
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
 });
